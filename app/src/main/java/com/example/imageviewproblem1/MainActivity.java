9c297bcb93f8278d2970456bd6f14867e26c822d
Andriy <ak8022@bs.amalnet.k12.il>
2020-02-29 17:28:52 +0200

    Image 1: London Eye

--- app/src/main/java/com/example/imageviewproblem1/MainActivity.java
+++ app/src/main/java/com/example/imageviewproblem1/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.imageviewproblem1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Random;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    ImageView myImageView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImageView=(ImageView)findViewById(R.id.imageView2);
     }
 
     public void buttonPressed(View view)
     {
-
+        Random myRandom=new Random();
+        int x=myRandom.nextInt(3);
+        if (x==0)
+        {
+            myImageView.setImageResource(R.drawable.londoneye);
+        }
     }
 }
